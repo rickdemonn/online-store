31152285b4d65760fceb5f3adfee0805eccfdf61
Dmitriy Denysko <dmytro.denysko@pivdenny.ua>
2020-07-21 17:52:16 +0300

    wip cart implementation

--- src/main/js/cart.js
+++ src/main/js/cart.js
@@ -24,12 +24,12 @@ const addProductToCart = event => {
 
 	let isInAlreadyInCart = false;
 	cart.forEach(item => {
-		if(item.productId === productId){
+		if (item.productId === productId) {
 			item.count++;
 			isInAlreadyInCart = true;
 		}
 	})
-	if (!isInAlreadyInCart){
+	if (!isInAlreadyInCart) {
 		cart.push({productId: productId, count: 1});
 	}
 
@@ -49,25 +49,77 @@ const calculateCountOfAllProductsInCart = (cart) => {
 }
 
 const cartListener = () => {
-	$('.ti-shopping-cart').click(createModal);
+	$('.ti-shopping-cart').click(showModalCart);
 }
 
-const createModal = event => {
+const showModalCart = event => {
 	const overlay = $('#overlay');
 	const close = $('.modal_close, #overlay');
 	const modal = $('.modal_div');
 	event.preventDefault();
 
-	overlay.fadeIn(400, () => { // пoсле oкoнчaния пoкaзывaния oверлэя
-				$(modal).css('display', 'block').animate({opacity: 1, top: '50%'}, 200);
-			});
+	overlay.fadeIn(400, () => {
+		$(modal).css('display', 'block').animate({opacity: 1, top: '50%'}, 200);
+	});
+
+	fillCart();
 
 	close.click(() => {
-		modal.animate({opacity: 0, top: '45%'}, 200, // плaвнo прячем
-						() => { // пoсле этoгo
-							$(this).css('display', 'none');
-							overlay.fadeOut(400); // прячем пoдлoжку
-						}
-				);
+		modal.animate({opacity: 0, top: '45%'}, 200, () => {
+					$(this).css('display', 'none');
+					overlay.fadeOut(400);
+				}
+		);
 	})
+
+
+}
+
+const fillCart = () => {
+	const productsInCart = JSON.parse(localStorage.getItem('cart'));
+	$('#modal-cart-block').text('');
+	fetch(productsUrl)
+			.then(res => res.json())
+			.then(res => {
+				let isEmptyCart = true;
+				res.forEach(product => {
+					productsInCart.forEach(productInCart => {
+						if(product.id === productInCart.productId) {
+							isEmptyCart = false;
+							createBlockWithProductInCart(product, productInCart.count);
+						}
+					})
+				})
+				if(isEmptyCart) {
+					$('<div/>', {text: 'Cart is empty'})
+				} else {
+					$('<button/>', {
+						type: 'button',
+						text: 'place an order'
+					}).click(placeAnOrder).appendTo($('#modal-cart-block'));
+				}
+			});
+}
+
+const createBlockWithProductInCart = (product, count) => {
+	const { productId, brand, model, price, img } = product;
+	const productCard = $('<div/>', {'data-id': productId}).addClass('product-in-cart-card').appendTo($('#modal-cart-block'));
+	$(`<img src="${img}">`).addClass('img-in-cart').appendTo(productCard);
+	$('<div>',{text: `${brand} ${model} `}).appendTo(productCard);
+	$('<div>',{text: `Count x ${count}`}).appendTo(productCard);
+	$('<div>',{text: `Price is ${price * count}`}).appendTo(productCard);
+	$('<button/>',{type: 'button', text: '-'}).click(removeOneProductFromCart).appendTo(productCard);
+	$('<button/>',{type: 'button', text: '+'}).click(addOneProductToCart).appendTo(productCard);
+}
+
+const removeOneProductFromCart = event => {
+	//TODO
+}
+
+const addOneProductToCart = event => {
+	//TODO
+}
+
+const placeAnOrder = event => {
+	//TODO форма для заказа товаров из корзины с волидацией
 }
