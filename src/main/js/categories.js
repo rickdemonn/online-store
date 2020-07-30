c1850704e4e802d70d759eaf381850142fd646c4
dima.denysko <36486204+rickdemonn@users.noreply.github.com>
2020-07-30 15:15:18 +0300

    categories rendering and switching between "pages" suggestion (#2)
    Co-authored-by: sidorina-katerenko <sidorina.katerenko@MacBook-Air-Kateryna.local>

--- src/main/js/categories.js
+++ src/main/js/categories.js
@@ -3,12 +3,33 @@ const categoriesStartListener = () => {
     $('.button-hero').click(createCategories);
 };
 
+// const createCategories = event => {
+//     event.preventDefault();
+//     $('.hero-banner').remove();
+//     $('#categories-block').remove();
+//     $('#products-block').remove();
+//     const categories = $('<div/>', {id: 'categories-block'}).prependTo($('.site-main'));
+//     categories.append($('<div/>').addClass('container'));
+//     fetch(categoriesUrl)
+//         .then(res => {
+//             return res.json();
+//         })
+//         .then(res => {
+//             createBlockOfCategories(res);
+//         })
+//         .catch(reject => {
+//             console.log("Ups");
+//             console.log(reject);
+//         });
+// };
+
 const createCategories = event => {
     event.preventDefault();
-    $('.hero-banner').remove();
-    $('#categories-block').remove();
-    $('#products-block').remove();
-    const categories = $('<div/>', {id: 'categories-block'}).prependTo($('.site-main'));
+
+    $('.hero-banner').removeClass('active');
+    $('.category-page').addClass('active');
+
+    const categories = document.querySelector('.category-list');
     categories.append($('<div/>').addClass('container'));
     fetch(categoriesUrl)
         .then(res => {
@@ -25,10 +46,37 @@ const createCategories = event => {
 
 const createBlockOfCategories = (response) => {
     response.forEach(categoryItem => {
+        const parent = document.querySelector('.category-list .row');
+        
         const {id, name, description, img} = categoryItem;
-        const card = $('<div/>', {'data-id': id}).addClass('category-card').click(showProductsByCategory).appendTo($('#categories-block > .container'));
-        card.append($('<img>',{'src': img}))
-            .append($('<div/>', {text: name}))
-            .append(($('<div/>', {text: description})));
+        const card = document.createElement('div');
+        card.classList.add("card", "card-product", "col-md-6", "col-lg-4");
+
+        const image = document.createElement('img');
+        image.setAttribute('src', img);
+
+        const textContainer = document.createElement('div');
+        textContainer.classList.add('card-body', 'text-center');
+
+        const productName = document.createElement('h4');
+        productName.textContent = name;
+
+        const productDescription = document.createElement('p');
+        productDescription.className = 'card-product__price';
+        productDescription.textContent = description;
+
+        card.appendChild(image);
+        textContainer.appendChild(productName);
+        textContainer.appendChild(productDescription);
+        card.appendChild(textContainer);
+
+        card.addEventListener('click', showProductsByCategory)
+
+        parent.appendChild(card);
+        
+        // $('<div/>', {'data-id': id}).addClass('card').addClass('card-product').click(showProductsByCategory).appendTo($('.category-list'));
+        // card.append($('<img>',{'src': img}).addClass('card-img'))
+        //     .append($('<div/>', {text: name}))
+        //     .append(($('<div/>', {text: description})));
     })
 };
